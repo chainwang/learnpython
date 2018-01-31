d1744dc56e9e0fdf666fd35cc549f5a0c9df9c5c
nono <498957566@qq.com>
2018-01-31 19:10:44 +0800

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+content = 'first of all,i want make it clear that i can not claim understanding this holy book in just'
+res = {}
+for s in content:
+	if s == ' ':
+		continue
+	if s not in res:
+		res[s] = 1
+	res[s] += 1
+res_list = []
+for k in res.keys():
+	res_list.append([k,res[k]])
+for i in range(10):
+	for j in range(len(res_list) - 1 -i):
+		if res_list[j][1] > res_list[j+1][1]:
+			res_list[j],res_list[j+1] = res_list[j+1],res_list[j]
+for r in res_list[-10:]:
+	print 'char %s count is %s' % (r[0],r[1])
